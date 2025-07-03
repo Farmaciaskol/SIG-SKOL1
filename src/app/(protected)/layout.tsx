8896705948f6c8a2e8a4149aa8a5bc728285ba6d
Ventas Farmacia Skol <ventas@farmaciaskol.cl>
2025-07-03 19:57:17 +0000

    el autentificador no esta funcionando

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,9 +1,12 @@
 import { MainNav } from '@/components/app/main-nav';
+import { AuthProvider } from '@/components/app/auth-provider';
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return (
-      <MainNav>
-        {children}
-      </MainNav>
+      <AuthProvider>
+        <MainNav>
+          {children}
+        </MainNav>
+      </AuthProvider>
   );
 }
