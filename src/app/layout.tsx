0e379bf9586ffb37b9178d64f8aa00fda2f52d99
Ventas Farmacia Skol <ventas@farmaciaskol.cl>
2025-07-04 12:34:15 +0000

    Puedes hacer que las letras de los titulos de las apmicaciones tenga la

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter, Space_Grotesk } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
@@ -7,12 +7,6 @@ const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-body',
-});
-
-const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-headline',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={`${inter.variable} ${spaceGrotesk.variable} h-full bg-background`}>
+    <html lang="en" suppressHydrationWarning className={`${inter.variable} h-full bg-background`}>
       <body className="h-full font-body antialiased">
         {children}
         <Toaster />
