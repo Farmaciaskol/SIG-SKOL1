89db20d9a4c29f4d3be410e254c87d55508b91b9
Ventas Farmacia Skol <ventas@farmaciaskol.cl>
2025-07-07 18:24:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/app/inventory-client.tsx
+++ src/components/app/inventory-client.tsx
@@ -31,6 +31,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
+  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog"
@@ -829,3 +830,5 @@ export function InventoryClient({ initialInventory }: {
         </>
     );
 }
+
+    
